71e41b9770fcf238ff2437be86ab6474624a6aa9
MapYh <145336421+MapYh@users.noreply.github.com>
2024-10-15 20:04:42 +0200

    Added some eslint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
   pluginReact.configs.flat.recommended,
 ];
